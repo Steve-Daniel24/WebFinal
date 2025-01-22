a6520a643a9c542a5237ddfb63b7778efffea8a5
NyVoary <93976218+NyVoary@users.noreply.github.com>
2025-01-22 20:31:00 +0300

    Add initial data for Clients and Admins in data.sql

---
+++ database/data.sql
@@ -0,0 +1,9 @@
+-- Inserting Clients
+INSERT INTO Clients (name, password, email, phone) VALUES 
+('John Doe', '123', 'john@example.com', '1234'),
+('Jane Smith', '456', 'jane@example.com', '1234'),
+('Alice Johnson', '789', 'alice@example.com', '1234');
+
+-- Inserting Admins
+INSERT INTO Admins (name, password) VALUES 
+('Ny Voary', 'ETU003657');
