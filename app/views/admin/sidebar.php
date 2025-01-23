37b342b445738f01aeb13ac8a6bba84ea53f0d77
NyVoary <93976218+NyVoary@users.noreply.github.com>
2025-01-23 22:22:20 +0300

    Add sidebar navigation items for Properties, Bookings, Clients, and Payments in admin view

--- app/views/admin/sidebar.php
+++ app/views/admin/sidebar.php
@@ -7,7 +7,34 @@
       <span>Dashboard</span>
     </a>
   </li>
-  
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#">
+      <i class="bi bi-grid"></i>
+      <span>Properties</span>
+    </a>
+  </li>
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#">
+      <i class="bi bi-grid"></i>
+      <span>Bookings</span>
+    </a>
+  </li>
+
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#">
+        <i class="bi bi-grid"></i>
+        <span>Clients</span>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#">
+        <i class="bi bi-grid"></i>
+        <span>Payments</span>
+        </a>
+    </li>
 </ul>
 
 </aside>
