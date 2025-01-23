39bfdf118065ec04bdb858a2a143e8124b74a9db
NyVoary <93976218+NyVoary@users.noreply.github.com>
2025-01-23 23:03:47 +0300

    Last changes of 23/01

--- app/views/clients/base.php
+++ app/views/clients/base.php
@@ -4,32 +4,31 @@
       <?php include 'head.php'; ?>
     </head>
 
-<body class="index-page">
-    <?php include 'header.php'; ?>
+    <body class="index-page">
+        <?php include 'header.php'; ?>
 
-        <!-- ITO ILAY MIOVA -->
-        <main class="main">
-            <?= $content ?>
-        </main>
 
-  <?php include 'footer.php'; ?>
+            <main class="main">
+                <?php include 'swipe.php'; ?>
+                <?= $content ?> <!-- ITO ILAY MIOVA -->
+            </main>
 
-  <!-- Scroll Top -->
-  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+      <?php include 'footer.php'; ?>
 
-  <!-- Vendor JS Files -->
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-  <script src="assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+      <!-- Scroll Top -->
+      <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-  <!-- Main JS File -->
-  <script src="assets/js/main.js"></script>
+      <!-- Vendor JS Files -->
+      <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+      <script src="assets/vendor/php-email-form/validate.js"></script>
+      <script src="assets/vendor/aos/aos.js"></script>
+      <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+      <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+      <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+      <script src="assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
+      <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
 
-</body>
-
+      <!-- Main JS File -->
+      <script src="assets/js/main.js"></script>
+    </body>
 </html>
